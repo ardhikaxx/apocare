475d418e88c6263bfe712ef3d476e7f2f688c826
ardhikaxx <ardhikayanuar58@gmail.com>
2026-02-15 06:03:55 +0700

    fix(breadcrumb) sembunyikan dashboard breadcrumb di halaman dashboard

--- resources/views/partials/breadcrumb.blade.php
+++ resources/views/partials/breadcrumb.blade.php
@@ -1,9 +1,12 @@
-﻿@php
+@php
     $breadcrumbs = $breadcrumbs ?? [];
+    $showDashboard = !(count($breadcrumbs) === 1 && isset($breadcrumbs[0]['label']) && $breadcrumbs[0]['label'] === 'Dashboard');
 @endphp
 <nav aria-label="breadcrumb" class="mb-3">
     <ol class="breadcrumb">
+        @if($showDashboard)
         <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+        @endif
         @foreach($breadcrumbs as $crumb)
             @if(isset($crumb['url']))
                 <li class="breadcrumb-item"><a href="{{ $crumb['url'] }}">{{ $crumb['label'] }}</a></li>
