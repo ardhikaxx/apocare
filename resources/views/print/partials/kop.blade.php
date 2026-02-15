05248a396d0a5378ca35b3b275a5af489d91f151
ardhikaxx <ardhikayanuar58@gmail.com>
2026-02-15 10:25:28 +0700

    fix(print) use table layout for kop header in PDF exports

--- resources/views/print/partials/kop.blade.php
+++ resources/views/print/partials/kop.blade.php
@@ -2,15 +2,19 @@
     $logoPath = public_path('assets/brand/apocare-logo.png');
     $logoExists = file_exists($logoPath);
 @endphp
-<div class="kop">
-    @if ($logoExists)
-        <div class="kop-logo">
-            <img src="{{ $logoPath }}" alt="Apocare Logo">
-        </div>
-    @endif
-    <div class="kop-text">
-        <div class="kop-title">APOCARE</div>
-        <div class="kop-subtitle">Integrated Pharmacy Management System</div>
-    </div>
-</div>
-<div class="kop-line"></div>
+<table style="width: 100%; border: none; margin-bottom: 8px;">
+    <tr>
+        @if ($logoExists)
+        <td style="width: 70px; vertical-align: middle;">
+            <img src="{{ $logoPath }}" alt="Apocare Logo" style="height: 65px; width: auto;">
+        </td>
+        <td style="vertical-align: middle; padding-left: 16px;">
+        @else
+        <td style="vertical-align: middle;">
+        @endif
+            <div style="font-size: 20px; font-weight: 700; letter-spacing: 1px;">APOCARE</div>
+            <div style="font-size: 12px; color: #444;">Integrated Pharmacy Management System</div>
+        </td>
+    </tr>
+</table>
+<div style="border-bottom: 1px solid #222; margin-bottom: 16px;"></div>
